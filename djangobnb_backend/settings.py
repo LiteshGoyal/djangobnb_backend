6220ddb2c760b8f4fe511be5a99c0e3913b10110
Sameer <liteshgoyal55@gmail.com>
2026-04-29 21:37:21 +0530

    Updated settings for deployment

--- djangobnb_backend/settings.py
+++ djangobnb_backend/settings.py
@@ -1,6 +1,9 @@
 import os
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -69,11 +72,18 @@ REST_FRAMEWORK = {
     )
 }
 
-CORS_ALLOWED_ORIGINS =[
-    "http://127.0.0.1:8000",
-    "http://127.0.0.1:3000"
-]
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS =[
+#     "http://127.0.0.1:8000",
+#     "http://127.0.0.1:3000"
+# ]
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    "CSRF_TRUSTED_ORIGINS",
+    ""
+).split(" ")
+
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
+
+CORS_ALLOW_ALL_ORIGINS = False
 
 REST_AUTH={
     "USE_JWT":True,
